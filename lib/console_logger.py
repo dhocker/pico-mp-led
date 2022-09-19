7a7fc59d841d8b45cef91693fbedec01ebf7e21c
Dave Hocker <AtHomeX10@gmail.com>
2022-09-19 10:25:12 -0500

    Handle console logging when no host is connected

--- lib/console_logger.py
+++ lib/console_logger.py
@@ -1,11 +1,15 @@
 from logger_device import LoggerDevice
 import utime
+from rpico_board import is_host_connected
 
 
 class ConsoleLogger(LoggerDevice):
+    HOST_CONNECTED = is_host_connected()
+
     def __init__(self):
         super().__init__()
 
     def print(self, level, logdata):
-        t = utime.localtime()
-        print(f"{t[3]}:{t[4]:02d}:{t[5]:02d} {level}:{logdata}")
+        if ConsoleLogger.HOST_CONNECTED:
+            t = utime.localtime()
+            print(f"{t[3]}:{t[4]:02d}:{t[5]:02d} {level}:{logdata}")
